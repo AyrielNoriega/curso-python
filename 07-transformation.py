dca39d95b5710e2184a207670450155794376a38
AyrielNoriega <ayriel.rentek@rentek.com.co>
2024-03-06 17:22:03 -0500

    Transformación de tipos

---
+++ 07-transformation.py
@@ -0,0 +1,7 @@
+'''
+no podemos conctenar string con numeros, como en javascript, hay que hacer una transformacion:
+'''
+print('Mi edad es ' + str(12))
+
+'''otra forma de hacer lo anterior es con format, asi:'''
+print(f'Mi edad es {12}' )
