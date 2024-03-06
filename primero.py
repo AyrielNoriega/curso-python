8e31016d2be45d8efc3df7d87f886c423cd3cf0b
AyrielNoriega <ayriel.rentek@rentek.com.co>
2024-03-06 16:31:41 -0500

    Variables

--- primero.py
+++ primero.py
@@ -11,3 +11,11 @@ comentario de varias lineas
 otro comentario de varias lineas
 '''
 
+# variables
+my_name = 'ayriel'
+age = 32
+# print(my_name)
+
+tu_nombre = input('cual es tu nombre?')
+print('el nobmre es', tu_nombre)
+
