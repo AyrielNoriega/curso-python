f92517d58a2ec103e40b521959edbbb871400178
AyrielNoriega <ayriel.rentek@rentek.com.co>
2024-03-06 18:59:34 -0500

    manejo de strings

---
+++ 14-string-recargado.py
@@ -0,0 +1,30 @@
+# búsqueda de textos
+
+text = 'ella sabe programar Python'
+print('JS' in text)
+print('Python' in text)
+print('python' in text)
+
+# evaluar tamaño de string
+print(len(text))
+
+# texto a mayúsculas
+print(text.upper())
+
+print(text.lower())
+
+print(text.count('a'))
+
+print(text.swapcase())
+
+print(text.startswith('Ella'))
+
+print(text.endswith('Ella'))
+
+print(text.replace('Ella', 'El'))
+
+print(text.capitalize())
+
+print(text.title())
+
+print("23".isdigit())
