d81419b29933b5cae52148c8bfb7e06eaa55959a
AyrielNoriega <ayriel.rentek@rentek.com.co>
2024-03-06 17:11:09 -0500

    numbers

---
+++ 05-numbers.py
@@ -0,0 +1,20 @@
+# int
+lives = 3
+print(type(lives))
+
+# float
+temperatura = 3.1
+print(type(temperatura))
+
+#resta
+lives -= 1
+print(lives)
+
+lives += 1
+print(lives)
+
+valorString = '23'
+
+valorNum = int(valorString)
+print(type(valorNum))
+
