ebb0d04e094827a596416ad74f35594790ec7b8b
AyrielNoriega <ayriel.rentek@rentek.com.co>
2024-03-06 17:28:21 -0500

    operadores aritméticos

---
+++ 08-arith.py
@@ -0,0 +1,18 @@
+# operadores aritméticos
+
+print(10 + 2)
+print(10 - 2)
+print(10 * 2)
+print(10 / 2)
+print(10 % 2)
+
+'''captura el numero entero de una division'''
+print(10 // 2)
+
+print(10 ** 2)
+
+
+print('Hola ' + 'Mundo')
+
+'''repite 3 veces el string'''
+print('Hola ' * 3)
