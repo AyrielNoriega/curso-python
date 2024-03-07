d921152e36d6485af23a171eccad8f015b28ceae
AyrielNoriega <ayriel.rentek@rentek.com.co>
2024-03-07 12:11:47 -0500

    handle dict

---
+++ 19-dict.py
@@ -0,0 +1,23 @@
+# 
+
+myDict = {
+    'name': 'Ayriel',
+    'lastName': 'Noriega',
+    'langs': [
+        'php',
+        'javascript'
+    ]
+}
+
+print(myDict)
+print(type(myDict))
+
+print(myDict['name'])
+
+
+print(len(myDict))
+
+print(myDict.get('namee'))
+
+print('name' in myDict)
+
