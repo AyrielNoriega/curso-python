15927b39d662c1f9a58df1eb797bec3813c78177
AyrielNoriega <ayriel.rentek@rentek.com.co>
2024-03-08 15:21:23 -0500

    Reto: map con inmutabilidad

--- 32-map-dicts.py
+++ 32-map-dicts.py
@@ -24,3 +24,13 @@ def addTaxes(item):
 
 newItems = list(map(addTaxes, items))
 print(newItems)
+
+'''no modificar el estado del array original con map'''
+def addTaxes(item):
+    newItem = item.copy()
+    newItem['taxes'] = newItem['price'] * .19 ##creamos nuevo item, con eso no modificados el array principal
+    return newItem
+
+newItems = list(map(addTaxes, items))
+print(items)
+print(newItems)
