457e9155ca91826ea3b453802995de28ba3df0e2
AyrielNoriega <ayriel.rentek@rentek.com.co>
2024-03-08 17:36:35 -0500

    Módulos como scripts: __name__ y __main__

---
+++ app/example.py
@@ -0,0 +1,5 @@
+import main
+
+print(main.otherData)
+
+# main.run()
--- app/main.py
+++ app/main.py
@@ -1,9 +1,24 @@
 import utils
 
-keys, values = utils.getPopulation()
-print(keys, values)
+def run(): ##con esta funcion podemos evitar que ejecute todo el archivo al hacer una importacion de main desde otro lado
+    keys, values = utils.getPopulation()
+    print(keys, values)
 
-data = [
+    data = [
+        {
+            'country': 'Colombia',
+            'population': 300
+        },
+        {
+            'country': 'Bolivia',
+            'population': 500
+        }
+    ]
+
+    res = utils.populationByCountry(data, 'Colombia')
+    print(res)
+
+otherData = [
     {
         'country': 'Colombia',
         'population': 300
@@ -14,5 +29,5 @@ data = [
     }
 ]
 
-res = utils.populationByCountry(data, 'Colombia')
-print(res)
+if __name__ == '__main__':
+    run()
