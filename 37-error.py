9304203f53bda3c9b0d2bb20c1fabe05d3ac0840
AyrielNoriega <anoriega2310@gmail.com>
2024-03-10 08:49:10 -0500

    Manejo de excepciones

--- 37-error.py
+++ 37-error.py
@@ -1,6 +1,18 @@
 suma = lambda x,y : x + y
 assert suma(2, 2) == 4
+assert suma(2, 2) == 4, 'No es igual'
 
 age = 10
-if age < 18:
+if age > 18:
     raise Exception('No se permite menors a 18')
+
+try:
+    print(0/0)
+except ZeroDivisionError as error:
+    print(error)
+
+try:
+    if age < 18:
+        raise Exception('No se permite menors a 18')
+except Exception as error:
+    print(error)
